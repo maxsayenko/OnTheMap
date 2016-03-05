2d9dee685e29a0922ad789b8083bbb686ec053c6
Max Saienko <msaienko@ancestry.com>
2016-03-05 12:07:53 -0800

    initial impl

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -10,28 +10,33 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    
     @IBOutlet var emailText: UITextField!
     
     @IBOutlet var passwordText: UITextField!
     
-    
-    
     @IBAction func loginPressed(sender: UIButton) {
+        getSession(emailText.text!, password: passwordText.text!)
     }
     
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    func getSession(email: String, password: String) {
+        guard (!email.isEmpty || !password.isEmpty) else {
+            print("Error")
+            return
+        }
+        
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
+        request.HTTPMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.HTTPBody = "{\"udacity\": {\"username\": \"\(email)\", \"password\": \"\(password)\"}}".dataUsingEncoding(NSUTF8StringEncoding)
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+            if error != nil { // Handle error…
+                return
+            }
+            let newData = data!.subdataWithRange(NSMakeRange(5, data!.length - 5)) /* subset response data! */
+            print(NSString(data: newData, encoding: NSUTF8StringEncoding))
+        }
+        task.resume()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
