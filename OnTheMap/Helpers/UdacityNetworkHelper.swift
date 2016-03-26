7d56cafe61389b947ffa4728f84bc5f97083d3c9
Max Saienko <msaienko@ancestry.com>
2016-03-26 15:46:43 -0700

    abstracting network code more

--- OnTheMap/Helpers/UdacityNetworkHelper.swift
+++ OnTheMap/Helpers/UdacityNetworkHelper.swift
@@ -9,6 +9,7 @@ import Foundation
 
 struct UdacityNetworkHelper {
     static let EMPTY_EMAIL_PASSWORD = "Email and password are required"
+    static let BAD_DATA = "Error in reading response"
 
     static func getUdacitySession(email: String, password: String, completionHandler: (data: AnyObject?, errorString: String?) -> Void) {
         guard (!email.isEmpty && !password.isEmpty) else {
@@ -16,30 +17,47 @@ struct UdacityNetworkHelper {
             return
         }
         
-        let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
-        request.HTTPMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.HTTPBody = "{\"udacity\": {\"username\": \"\(email)\", \"password\": \"\(password)\"}}".dataUsingEncoding(NSUTF8StringEncoding)
-        let session = NSURLSession.sharedSession()
-        let task = session.dataTaskWithRequest(request) { data, response, error in
-            if error != nil { // Handle error…
-                return
-            }
+        // TODO: Move Url to Config class
+        let data = "{\"udacity\": {\"username\": \"\(email)\", \"password\": \"\(password)\"}}"
+        networkPostRequest("https://www.udacity.com/api/session", data: data) { data, errorString in
             
             /* subset response data! */
             let newData = data!.subdataWithRange(NSMakeRange(5, data!.length - 5))
-            
             var parsedData: AnyObject
             
             do {
                 parsedData = try NSJSONSerialization.JSONObjectWithData(newData, options: .AllowFragments)
             } catch {
-                print("Error in parsing JSON \(error)")
+                completionHandler(data: nil, errorString: BAD_DATA)
                 return
             }
             
-            completionHandler(data: parsedData, errorString: nil)
+            // Got error response from Udacity. Terminate.
+            if let error = parsedData["error"] as? String {
+                completionHandler(data: nil, errorString: error)
+                return
+            }
+            
+            print(parsedData)
+            print(errorString)
+        }
+    }
+    
+    private static func networkPostRequest(url: String, data: String, completionHandler: (data: AnyObject?, errorString: String?) -> Void) {
+        let request = NSMutableURLRequest(URL: NSURL(string: url)!)
+        request.HTTPMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.HTTPBody = data.dataUsingEncoding(NSUTF8StringEncoding)
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+            // Got an Error. Terminate.
+            if (error != nil) {
+                completionHandler(data: nil, errorString: error?.localizedDescription)
+                return
+            }
+            
+            completionHandler(data: data, errorString: nil)
         }
         
         task.resume()
