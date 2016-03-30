7dbce1110d7925c5c011051d64ef6fa4c65cf355
Max Saienko <msaienko@ancestry.com>
2016-03-29 23:02:58 -0700

    MapVC

---
+++ OnTheMap/TabBar/MapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewController.swift
+//  OnTheMap
+//
+//  Created by Max Saienko on 3/29/16.
+//  Copyright © 2016 Max Saienko. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+}
