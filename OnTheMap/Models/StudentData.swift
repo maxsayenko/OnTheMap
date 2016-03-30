7b421dc8a9ee73f10c708662951db970d4b31912
Max Saienko <msaienko@ancestry.com>
2016-03-30 00:19:04 -0700

    adding StudentData model

---
+++ OnTheMap/Models/StudentData.swift
@@ -0,0 +1,20 @@
+//
+//  StudentData.swift
+//  OnTheMap
+//
+//  Created by Max Saienko on 3/30/16.
+//  Copyright © 2016 Max Saienko. All rights reserved.
+//
+
+struct StudentData {
+    let createdAt: String
+    let firstName: String
+    let lastName: String
+    let latitude: Float
+    let longitude: Float
+    let mapString: String
+    let mediaURL: String
+    let objectId: String
+    let uniqueKey: String
+    let updatedAt: String
+}
