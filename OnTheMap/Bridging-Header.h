23f3496e6d024b221811902c8ce7550732fea03e
Max Saienko <msaienko@ancestry.com>
2016-04-23 16:04:06 -0700

    finish setting up FBSDK

---
+++ OnTheMap/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  OnTheMap
+//
+//  Created by Max Saienko on 4/23/16.
+//  Copyright © 2016 Max Saienko. All rights reserved.
+//
+
+#ifndef FacebookTutorial_Bridging_Header_h
+#define FacebookTutorial_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
+#endif
